10818aa07d7128006d56f1db35e4e27daa4234e7
Jonathan <jcarrenog8@gmail.com>
2018-06-05 16:54:28 -0500

    Segundo commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RINGTONES } from "../../data/data.ringtones";
 import { Ringtone } from "../../interfaces/ringtone.interface";
+import { Refresher, reorderArray } from "ionic-angular";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ import { Ringtone } from "../../interfaces/ringtone.interface";
 export class HomePage {
 
   ringtones:Ringtone[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+  ordenando:boolean = false;
 
   constructor() {
 
@@ -18,18 +22,75 @@ export class HomePage {
 
   reproducir( ringtone: Ringtone ){
 
+    this.pausar_audio( ringtone );
+
+    if ( ringtone.resproduciendo == true ) {
+     
+      ringtone.resproduciendo = false;
+      return;
+      
+    }
+
     console.log( ringtone );
+    
+    this.audio.src = ringtone.audio;
 
-    let audio = new Audio();
-    audio.src = ringtone.audio;
-
-    audio.load();
-    audio.play();
+    this.audio.load();
+    this.audio.play();
     
     ringtone.resproduciendo = true;
 
-    setTimeout( ()=> ringtone.resproduciendo = false, ringtone.duracion * 1000 );
+    this.audioTiempo = setTimeout( ()=> ringtone.resproduciendo = false, ringtone.duracion * 1000 );
 
   }
+
+  private pausar_audio( rintonSel:Ringtone ){
+
+    clearTimeout( this.audioTiempo );
+
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for( let ringtone of this.ringtones ){
+
+      if( ringtone.nombre != rintonSel.nombre ){
+
+        ringtone.resproduciendo = false;
+
+      }
+
+    }
+
+  }
+
+  borrar_ringtone( idx:number ){
+
+    this.ringtones.splice( idx, 1);
+
+  }
+
+  recargar_ringtones( refresher:Refresher ){
+
+    console.log("Inicio del refresh");
+
+    setTimeout( ()=>{
+
+      console.log("Termino el refresh");
+      this.ringtones = RINGTONES.slice(0);  
+
+      refresher.complete();
+
+    }, 1500)
+
+  }
+
+  reordenar_ringtones( indices:any ){
+
+    console.log(indices);
+
+    this.ringtones = reorderArray( this.ringtones, indices );
+
+  }
+
 
 }
